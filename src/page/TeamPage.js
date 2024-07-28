51246253816036d3045761f798c0307087df7dfa
mmzkyyl <zzy1373106591@163.com>
2024-07-28 09:22:33 +0800

    成员介绍

--- src/page/TeamPage.js
+++ src/page/TeamPage.js
@@ -2,6 +2,7 @@ import Navbar from "../component/Navbar";
 
 import Background from "../img/team1.jpg"
 import ScrollToTopButton from "../component/ScrollToTopButton";
+import AllTeamers from "../store/TeamStore";
 
 const TeamPage = () => {
     return (
@@ -20,30 +21,19 @@ const TeamPage = () => {
                 </div>
             </div>
 
-            <div className="px-12">
-                <div className="text-2xl p-4">
-                    团队成员
-                </div>
-                <div className="text-2xl p-4">
-                    团队成员
-                </div>
-                <div className="text-2xl p-4">
-                    团队成员
-                </div>
-                <div className="text-2xl p-4">
-                    团队成员
-                </div>
-                <div className="text-2xl p-4">
-                    团队成员
-                </div>
-                <div className="text-2xl p-4">
-                    团队成员
-                </div>
-                <div className="text-2xl p-4">
-                    团队成员
-                </div>
-                <div className="text-2xl p-4">
-                    团队成员
+            <div className="h-6"></div>
+
+            <div className="p-6">
+                <div className="grid grid-cols-3 gap-12 px-44">
+                    {AllTeamers.map((item, index) => (
+                        <div key={index} className="relative w-full max-w-md">
+                            <img src={item.img} alt={item.title} className="w-full h-auto" style={{height: '60vh'}}/>
+                            <div
+                                className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100">
+                                <p className="text-white text-center">{item.intro}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
 
