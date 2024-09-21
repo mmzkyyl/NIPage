508360ea637e221d275a80e6b996dc7172debf14
mmzkyyl <zzy1373106591@163.com>
2024-09-21 09:30:01 +0800

    修改成果展示页面，添加切换模式

--- src/page/ResultPage.js
+++ src/page/ResultPage.js
@@ -4,8 +4,15 @@ import Background from "../img/result.png"
 import AllThoughts from "../store/MemoryStore";
 import AllLinks from "../store/LinkStore";
 import ScrollToTopButton from "../component/ScrollToTopButton";
+import {useState} from "react";
 
 const ResultPage = () => {
+    const [ifMaster, setIfMaster] = useState(false);
+
+    const toggleMode = () => {
+        setIfMaster(prev => !prev); // 切换 ifMaster 的值
+    };
+
     return (
         <>
             <Navbar/>
@@ -44,10 +51,18 @@ const ResultPage = () => {
                 </div>
 
                 <div className="w-2/12 border-l border-gray-300 pl-4">
-                    <div className="sticky top-0">
-                        <h2 className="text-xl font-bold mb-4">AI入口</h2>
+                    <div className="sticky top-20">
+                        <div>
+                            <h2 className="text-xl font-bold mb-4 flex-row inline-block">AI入口</h2>
+                            <button
+                                className="px-4 text-blue-400 inline-block"
+                                onClick={toggleMode}
+                            >
+                                切换模式({ifMaster ? '专家' : '普通'})
+                            </button>
+                        </div>
 
-                        <ul className="space-y-4">
+                        <ul className="space-y-2">
                             {AllLinks.map((link, index) => (
                                 <li>
                                     <a href={`${link.link}`} className="text-blue-500 hover:underline">
