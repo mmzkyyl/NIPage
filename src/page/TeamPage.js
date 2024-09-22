b7f0eb7ade8b99b84d649eedce15f9548136e524
mmzkyyl <zzy1373106591@163.com>
2024-09-22 20:57:29 +0800

    成员ui修改

--- src/page/TeamPage.js
+++ src/page/TeamPage.js
@@ -4,8 +4,11 @@ import Background from "../img/team1.jpg"
 import ScrollToTopButton from "../component/ScrollToTopButton";
 import AllTeamers from "../store/TeamStore";
 import AllBackground from "../img/background.png";
+import {useState} from "react";
 
 const TeamPage = () => {
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
     return (
         <>
             <Navbar/>
@@ -38,22 +41,36 @@ const TeamPage = () => {
             <div className="p-6 flex justify-center items-center">
                 <div className="grid grid-cols-3 gap-x-24 gap-y-16">
                     {AllTeamers.map((item, index) => (
-                        <div key={index} className="relative w-60 h-60">
-                            <img
-                                src={item.img}
-                                alt={item.title}
-                                className="w-full h-full rounded-full object-cover object-top"
-                            />
-                            <div
-                                className="rounded-full absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100"
-                            >
-                                <p className="text-white text-center text-xs">{item.intro}</p>
+                        <div
+                            key={index}
+                            className="relative w-60 h-80 border-2 border-gray-300 p-1 flex flex-col items-center"
+                            onMouseEnter={() => setHoveredIndex(index)}
+                            onMouseLeave={() => setHoveredIndex(null)}
+                        >
+                            <div className="w-32 h-32 mb-2">
+                                <img
+                                    src={item.img}
+                                    alt={item.title}
+                                    className="w-full h-full rounded-full object-cover object-top"
+                                />
+                            </div>
+                            <div className="text-center">
+                                <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
+                                <p
+                                    className={`text-gray-600 transition duration-500 ease-in-out ${
+                                        hoveredIndex === index ? 'text-sm' : 'text-lg'
+                                    }`}
+                                >
+                                    {hoveredIndex === index ? item.intro : item.introShort}
+                                </p>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
 
+
+            <div className="h-6"></div>
             <ScrollToTopButton/>
         </>
     );
--- src/store/TeamStore.js
+++ src/store/TeamStore.js
@@ -6,26 +6,26 @@ import ChenZhiheng from "../img/teamer/ChenZhiheng.jpg"
 import ZhouShengmi from "../img/teamer/ZhouShengmi.jpg"
 import ZhouYan from "../img/teamer/ZhouYan.jpg"
 
-const ZhouYvdongIntro = ['周渝东，武汉大学弘毅学堂数字经济试验班2022级本科生，湖北省“青马工程”第十一期学员，获武汉大学三好学生、优秀共青团员等校级荣誉十余项，曾获第十三届全国大学生电子商务“创新、创意及创业”挑战赛湖北省一等奖。'];
+const ZhouYvdongIntro = ['武汉大学弘毅学堂数字经济试验班2022级本科生，湖北省“青马工程”第十一期学员，获武汉大学三好学生、优秀共青团员等校级荣誉十余项，曾获第十三届全国大学生电子商务“创新、创意及创业”挑战赛湖北省一等奖。'];
 
-const XueYvchenIntro = ['薛煜辰，上海交通大学电子信息与电气工程学院自动化专业2022级本科生。参加美国大学生数学建模比赛、全国大学生电子设计大赛等大型比赛，有强烈的学术热情和创新思维。'];
+const XueYvchenIntro = ['上海交通大学电子信息与电气工程学院自动化专业2022级本科生。参加美国大学生数学建模比赛、全国大学生电子设计大赛等大型比赛，有强烈的学术热情和创新思维。'];
 
-const ZhuChenxiIntro = ['朱晨曦，信息管理学院编辑出版学（数字出版方向）2022级本科生。曾获得第十四届全国大学生电子商务“创新、创意及创业”挑战赛校级二等奖，武汉大学第二届信息可视化大赛二等奖，校社会实践大赛二等奖等。'];
+const ZhuChenxiIntro = ['信息管理学院编辑出版学（数字出版方向）2022级本科生。曾获得第十四届全国大学生电子商务“创新、创意及创业”挑战赛校级二等奖，武汉大学第二届信息可视化大赛二等奖，校社会实践大赛二等奖等。'];
 
-const ChenZhihengIntro = ['陈治衡，上海交通大学电子信息与电气工程学院自动化专业2022级本科生。曾获2023年美国大学生建模竞赛F奖、第二十八期“上海交通大学大学生创新实践计划”一等奖。'];
+const ChenZhihengIntro = ['上海交通大学电子信息与电气工程学院自动化专业2022级本科生。曾获2023年美国大学生建模竞赛F奖、第二十八期“上海交通大学大学生创新实践计划”一等奖。'];
 
-const ZhouShengmiIntro = ['周晟旻，武汉大学弘毅学堂数字经济试验班2022级本科生。曾获2023“外研社·国才杯”全国大学生外语能力大赛阅读赛项武汉大学一等奖。'];
+const ZhouShengmiIntro = ['武汉大学弘毅学堂数字经济试验班2022级本科生。曾获2023“外研社·国才杯”全国大学生外语能力大赛阅读赛项武汉大学一等奖。'];
 
-const ZhouYanIntro = ['周言，武汉大学历史学院2023级历史学类本科生，曾获武汉大学2024年社会活动积极分子、武汉大学2024年学生寒假社会实践活动团体一等奖、武汉大学第六届“观世界·论中国”大学生学术论坛作品评比三等奖等奖项。'];
+const ZhouYanIntro = ['武汉大学历史学院2023级历史学类本科生，曾获武汉大学2024年社会活动积极分子、武汉大学2024年学生寒假社会实践活动团体一等奖、武汉大学第六届“观世界·论中国”大学生学术论坛作品评比三等奖等奖项。'];
 
 const AllTeamers = [
-    { img: ZhouYvdong, intro: ZhouYvdongIntro },
-    { img: XueYvchen, intro: XueYvchenIntro },
+    { img: ZhouYvdong, name:"周渝东", intro: ZhouYvdongIntro, introShort: "武汉大学弘毅学堂数字经济试验班2022级本科生" },
+    { img: XueYvchen, name:"薛煜辰", intro: XueYvchenIntro, introShort: "上海交通大学电子信息与电气工程学院自动化专业2022级本科生" },
 
-    { img: ZhuChenxi, intro: ZhuChenxiIntro },
-    { img: ChenZhiheng, intro: ChenZhihengIntro },
-    { img: ZhouShengmi, intro: ZhouShengmiIntro },
-    { img: ZhouYan, intro: ZhouYanIntro },
+    { img: ZhuChenxi, name:"朱晨曦", intro: ZhuChenxiIntro, introShort: "信息管理学院编辑出版学（数字出版方向）2022级本科生" },
+    { img: ChenZhiheng, name:"陈治衡", intro: ChenZhihengIntro, introShort: "上海交通大学电子信息与电气工程学院自动化专业2022级本科生" },
+    { img: ZhouShengmi, name:"周晟旻", intro: ZhouShengmiIntro, introShort: "武汉大学弘毅学堂数字经济试验班2022级本科生" },
+    { img: ZhouYan, name:"周言", intro: ZhouYanIntro, introShort: "武汉大学历史学院2023级历史学类本科生" },
 ];
 
 export default AllTeamers;
